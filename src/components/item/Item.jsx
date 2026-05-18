c77ec35b2fa0fcf74cf0babddb5f40190013c5dc
JorgeGit-dev <132378583+JorgeGit-dev@users.noreply.github.com>
2026-05-18 18:57:34 -0300

    Mejor consistencia visual, legilibilidad, semantica del Item

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,29 +1,53 @@
 import "./Item.css";
 
-export const Item = ({ producto, marca, presentacion, peso, precio_kilo, precio_caja, descripcion, imagen, children }) => {
+export const Item = ({
+  producto,
+  marca,
+  presentacion,
+  peso,
+  precio_kilo,
+  precio_caja,
+  descripcion,
+  imagen,
+  children,
+}) => {
 
   const formatearPrecio = (precio) => {
-  if (typeof precio === "number") {
-    return precio.toLocaleString("es-AR", {
-      style: "currency",
-      currency: "ARS",
-    });
-  }
+    if (typeof precio === "number") {
+      return precio.toLocaleString("es-AR", {
+        style: "currency",
+        currency: "ARS",
+      });
+    }
 
-  return precio;
-};
+    return precio;
+  };
 
   return (
     <article className="card">
-     <img src={imagen} alt={`Detalle del producto ${producto}`} />
+      <img
+        src={imagen}
+        alt={`Detalle del producto ${producto}`}
+      />
+
       <h3>{producto}</h3>
-      <p>{marca}</p>
-      <p>{presentacion}</p>
-      <p>{peso}</p>
-      <p>Precio por kilo: {formatearPrecio(precio_kilo)}</p>
-      <p>Precio por caja: {formatearPrecio(precio_caja)}</p>
-      <p>{descripcion}</p>
-      
+
+      <p><strong>Marca:</strong> {marca}</p>
+      <p><strong>Presentación:</strong> {presentacion}</p>
+      <p><strong>Peso:</strong> {peso}</p>
+
+      <p>
+        <strong>Precio por kilo:</strong>{" "}
+        {formatearPrecio(precio_kilo)}
+      </p>
+
+      <p>
+        <strong>Precio por caja:</strong>{" "}
+        {formatearPrecio(precio_caja)}
+      </p>
+
+      <p><strong>Description:</strong> {descripcion}</p>
+
       {children}
     </article>
   );
